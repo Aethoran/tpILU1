8d0a26070e3025b1af85550b9496156061a44b52
etudianfsi <etudiantfsi@ens.fsi>
2024-09-27 09:42:47 +0200

    End Sess2

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,10 +3,16 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
-
+	private Equipement stuff[];
+	private int carryCapacity;
+	private int nbEquipement;
+	
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		this.nbEquipement = 0;
+		this.stuff = new Equipement[carryCapacity];
+		assert (force>=0);
 	}
 
 	public String getNom() {
@@ -23,17 +29,19 @@ public class Romain {
 
 	public void recevoirCoup(int forceCoup) {
 		force -= forceCoup;
+		assert (force>=0);
 		if (force > 0) {
 			parler("Aïe");
 		} else {
 			parler("J'abandonne...");
+			assert (force>=0);
 		}
 	}
 	
 	public static void main(String[] args) {
 		Romain loupailebus = new Romain ("Loupailebus", 2);
 		loupailebus.parler("sup");
-		
+		System.out.println(Equipement.CASQUE);
 		
 	}
 	
